29beea6385192652128337b0337ea64d86ce6723
NodePing <support@nodeping.com>
2019-12-02 16:21:43 -0800

    Changed version number to 1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DESCRIPTION = 'Python package for querying the NodePing API'
 URL = 'https://github.com/NodePing/python-nodeping-api'
 EMAIL = 'support@nodeping.com'
 AUTHOR = 'NodePing'
-VERSION = '0.9.9_4'
+VERSION = '1.0'
 LICENSE = 'MIT'
 
 setuptools.setup(
